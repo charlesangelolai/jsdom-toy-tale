76ba2df5861a8d670fa5f60fc478e9db29bc7c7c
Charles Lai <charlesangeloplai@gmail.com>
2021-03-01 06:27:18 -0500

    commit tree 5d6790907cec35680a1fdb5c2e4a23e69a3ae6f2

--- src/index.js
+++ src/index.js
@@ -13,3 +13,28 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function renderToys(toyData) {
+  const toyCollection = document.getElementById("toy-collection");
+
+  toyData.forEach((toy) => {
+    const div = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+    const btn = document.createElement("button");
+
+    div.classList.add("card");
+    h2.innerHTML = toy.name;
+    img.src = toy.image;
+    p.innerHTML = toy.likes;
+    btn.classList.add("like-btn");
+
+    div.append(h2, img, p, btn);
+    toyCollection.append(div);
+  });
+}
+
+fetch("http://localhost:3000/toys")
+  .then((resp) => resp.json())
+  .then((data) => renderToys(data));
